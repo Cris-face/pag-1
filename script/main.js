19c4f7126abc18ae733a3c6c650add1dc8250149
Cristian Carias <cariasperez09@outlook.com>
2022-12-05 23:48:36 -0600

    acutalización del hero

--- script/main.js
+++ script/main.js
@@ -1,4 +1,26 @@
 function cambiarModo() {
   var cuerpoweb = document.body;
-  cuerpoweb.classList.toggle("oscuro");
+  var icon = document.getElementById("sun");
+  var icon2 = document.getElementById("moon");
+
+  if (cuerpoweb = cuerpoweb.classList.toggle("oscuro")){
+    icon.style.display = 'block';
+    icon2.style.display = 'none';
+  }
+
+  else {
+    icon.style.display = 'none';
+    icon2.style.display = 'block';
+  }
+
+  /*switch (cuerpoweb.classList.toggle("oscuro")) {
+    case "oscuro":
+      icon.style.display = 'block';
+      icon2.style.display = 'none';
+      break;
+    case "":
+      icon.style.display = 'none';
+      icon2.style.display = 'block';
+      break;
+  }*/
 }
