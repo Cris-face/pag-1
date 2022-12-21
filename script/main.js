bd0f3e255e17f61af5884eafddd65a2dcca8895b
Cristian Carias <cariasperez09@outlook.com>
2022-12-21 14:33:21 -0600

    actualizar hero ask y footer

--- script/main.js
+++ script/main.js
@@ -1,9 +1,11 @@
+// _________________ change color theme __________________
+
 function cambiarModo() {
   var cuerpoweb = document.body;
   var icon = document.getElementById("sun");
   var icon2 = document.getElementById("moon");
 
-  if (cuerpoweb = cuerpoweb.classList.toggle("oscuro")){
+  if (cuerpoweb = cuerpoweb.classList.toggle("oscuro")) {
     icon.style.display = 'block';
     icon2.style.display = 'none';
   }
@@ -12,15 +14,18 @@ function cambiarModo() {
     icon.style.display = 'none';
     icon2.style.display = 'block';
   }
-
-  /*switch (cuerpoweb.classList.toggle("oscuro")) {
-    case "oscuro":
-      icon.style.display = 'block';
-      icon2.style.display = 'none';
-      break;
-    case "":
-      icon.style.display = 'none';
-      icon2.style.display = 'block';
-      break;
-  }*/
 }
+
+
+// _________________ scroll menu __________________
+
+const navEl = document.querySelector('.menu');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 56) {
+    navEl.classList.add('toggle-bg')
+  }
+  else {
+    navEl.classList.remove('toggle-bg')
+  }
+})
